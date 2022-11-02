68510f044c48ba740a3d47a3aed6b9de54b07ba7
SivaKrishna <shivataraya@gmail.com>
2022-11-02 19:09:20 +0530

    add health_check endpoint with tests setup

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+//! lib.rs
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async
+// without having to use any proc-macro incantation.
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new( || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
---
+++ tests/health_check.rs
@@ -0,0 +1,8 @@
+//! tests/health_check.rs
+
+use zero2prod::main;
+
+#[test]
+fn dummy_test() {
+  main()
+}
