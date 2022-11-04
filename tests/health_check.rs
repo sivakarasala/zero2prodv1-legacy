1a79c4666b48ca8014147e967621b10cd549e20d
SivaKrishna <shivataraya@gmail.com>
2022-11-04 17:54:47 +0530

    clean up

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,19 @@
 //! tests/health_check.rs
-
 use std::net::TcpListener;
 
-// `tokio::test` is the testing equivalent of `tokio::main`.
-// It also spares you from having to specify the `#[test]` attribute.
-//
-// You can insepect what code gets generated using
-// `cargo expand --test health_check` (<- name of the test file)
+/// Spin up an instance of our application
+/// and returns its address (i.e. http://localhost:xxxx)
+fn spawn_app() -> String {
+  let listener = TcpListener::bind("127.0.0.1:0")
+    .expect("Failed to bind random port");
+  // We retreive the port assigned to us by the OS
+  let port = listener.local_addr().unwrap().port();
+  let server = zero2prod::run(listener).expect("Failed to bind address");
+  let _ = tokio::spawn(server);
+  // We return the application address to the caller!
+  format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
   // Arrange
@@ -25,16 +32,4 @@ async fn health_check_works() {
   // Assert
   assert!(response.status().is_success());
   assert_eq!(Some(0), response.content_length());
-}
-
-// Launch our application in the background -somehow-
-fn spawn_app() -> String {
-  let listener = TcpListener::bind("127.0.0.1:0")
-    .expect("Failed to bind random port");
-  // We retreive the port assigned to us by the OS
-  let port = listener.local_addr().unwrap().port();
-  let server = zero2prod::run(listener).expect("Failed to bind address");
-  let _ = tokio::spawn(server);
-  // We return the application address to the caller!
-  format!("http://127.0.0.1:{}", port)
 }
