b39942cae2cbfa566c53a700ddca506e1a2f1b15
SivaKrishna <shivataraya@gmail.com>
2022-11-24 03:55:11 +0530

    add serde and hook subscribe handler to route reqs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,17 @@ use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
@@ -12,6 +22,8 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new( || {
             App::new()
                 .route("/health_check", web::get().to(health_check))
+                // A new entry in our routing table for POST /subscritpionss requests
+                .route("/subscriptions", web::post().to(subscribe))
         })
         .listen(listener)?
         .run();
